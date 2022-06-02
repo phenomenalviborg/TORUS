82482b53f8f64986508aa73545791068eaff8c8d
atomtwist <atomtwist@gmail.com>
2022-06-02 12:25:24 +0200

    Torus scene with VR setup

--- Assets/Scripts/Misc/CameraPosition.cs
+++ Assets/Scripts/Misc/CameraPosition.cs
@@ -4,15 +4,15 @@ public class CameraPosition : MonoBehaviour
 {
     private Transform trans;
     private static Vector3 pos;
-    
-    
+
+
     private void Start()
     {
         trans = transform;
         pos = trans.position;
     }
 
-   
+
     private void LateUpdate()
     {
         pos = trans.position;
