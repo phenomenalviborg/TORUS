3d2f9a7e57deb4743861c24bb98f1140cfb02e0b
atomtwist <atomtwist@gmail.com>
2022-06-13 22:22:08 +0200

    Horrible Oculus SDK hack with fingurs Networked

---
+++ Assets/Milan/QuestHandsForNormcore/Runtime/PlayerAvatar.cs
@@ -0,0 +1,65 @@
+﻿using ATVR;
+using UnityEngine;
+using Normal.Realtime;
+
+namespace absurdjoy
+{
+    public class PlayerAvatar : MonoBehaviour
+    {
+        public RealtimeTransform head;
+        public RealtimeTransform root;
+        public QuestSkeletonSerializer leftHand;
+        public QuestSkeletonSerializer rightHand;
+
+        private Transform localHead;
+        private Transform localRoot;
+        private Transform localLeftHand;
+        private Transform localRightHand;
+
+        private RealtimeView realtimeView;
+        
+        private void OnEnable()
+        {
+            realtimeView = GetComponent<RealtimeView>();
+        }
+        
+        /// <summary>
+        /// Called by the spawning computer (for the local avatar only). This is not called for remote avatars.
+        /// </summary>
+        public void LinkWithLocal(Transform localHead, OVRCustomSkeleton localLeftSkeleton, OVRCustomSkeleton localRightSkeleton, Transform localRoot)
+        {
+            // RealtimeTransform requires explicit ownership to update the position.
+            head.RequestOwnership();
+            root.RequestOwnership();
+            leftHand.GetComponent<RealtimeTransform>().RequestOwnership();
+            rightHand.GetComponent<RealtimeTransform>().RequestOwnership();
+
+            this.localRoot = localRoot;
+            this.localHead = localHead;
+            this.localLeftHand = localLeftSkeleton.transform.parent;
+            this.localRightHand = localRightSkeleton.transform.parent;
+            
+            leftHand.AssignLocalSkeleton(localLeftSkeleton);
+            rightHand.AssignLocalSkeleton(localRightSkeleton);
+        }
+
+        private void Update()
+        {
+            if (realtimeView.isOwnedLocallySelf)
+            {
+                // We need to manually set our avatar positions to match head/hand positions for the local stuff:
+                MatchTransform(localHead, head.transform);
+                MatchTransform(localLeftHand, leftHand.transform);
+                MatchTransform(localRightHand, rightHand.transform);
+                MatchTransform(localRoot, root.transform);
+            }
+        }
+
+        private void MatchTransform(Transform source, Transform target)
+        {
+            target.localPosition = source.localPosition;
+            target.localRotation = source.localRotation;
+            target.localScale = source.localScale;
+        }
+    }
+}
--- Assets/_Shared/_General/Assets.cs
+++ Assets/_Shared/_General/Assets.cs
@@ -6,176 +6,179 @@ using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
-
-public static class Assets
+namespace Andy
 {
-    public static T[] FindAll<T>() where T : Object
+    public static class Assets
     {
-        List<T> assets = new List<T>();
-        string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
-        for( int i = 0; i < guids.Length; i++ )
+        public static T[] FindAll<T>() where T : Object
         {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
+            List<T> assets = new List<T>();
+            string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
 
-            Object[] allObjects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+                Object[] allObjects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
 
-           T asset = AssetDatabase.LoadAssetAtPath<T>( assetPath );
-            if(asset != null)
-                assets.Add(asset);
+                T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                if (asset != null)
+                    assets.Add(asset);
+            }
+
+            return assets.ToArray();
         }
-        return assets.ToArray();
-    }
-    
-    
-    public static Mesh[] FindAllMeshes(string path, bool getAllInObject = false)
-    {
-        List<Mesh> assets = new List<Mesh>();
-        string[] guids = AssetDatabase.FindAssets("", new[] { "Assets/" + path });
-        List<string> used = new List<string>();
-        
-        for( int i = 0; i < guids.Length; i++ )
+
+
+        public static Mesh[] FindAllMeshes(string path, bool getAllInObject = false)
         {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-            if(used.Contains(assetPath))
+            List<Mesh> assets = new List<Mesh>();
+            string[] guids = AssetDatabase.FindAssets("", new[] {"Assets/" + path});
+            List<string> used = new List<string>();
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (used.Contains(assetPath))
+                    continue;
+
+                Object[] allObjects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+
+                for (int o = 0; o < allObjects.Length; o++)
+                    if (allObjects[o] is Mesh)
+                    {
+                        assets.Add((Mesh) allObjects[o]);
+                        used.Add(assetPath);
+
+                        if (!getAllInObject)
+                            break;
+                    }
+            }
+
+            return assets.ToArray();
+        }
+
+
+        public static T Get<T>(string name) where T : Object
+        {
+            string[] guids = AssetDatabase.FindAssets(name);
+
+            if (guids.Length == 0)
+            {
+                Debug.Log("Couldn't find Asset with Name \"" + name + "\"");
+                return null;
+            }
+
+            if (guids.Length > 1)
+            {
+                Debug.Log("Too many Assets with Name \"" + name + "\"");
+                return null;
+            }
+
+            string guid = guids[0];
+            T thing = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
+
+            if (thing == null)
+                Debug.Log("Assets with Name \"" + name + "\" isn't the right Type");
+
+            return thing;
+        }
+
+
+        public static Material[] FindAllMaterials(string path = "")
+        {
+            List<Material> assets = new List<Material>();
+            string[] guids = AssetDatabase.FindAssets("t:material");
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (!assetPath.Contains(path))
+                    continue;
+
+                Material asset = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+                if (asset != null)
+                    assets.Add(asset);
+            }
+
+            return assets.ToArray();
+        }
+
+
+        public static Texture2D[] FindAllTextures(string path = "")
+        {
+            List<Texture2D> assets = new List<Texture2D>();
+            string[] guids = AssetDatabase.FindAssets("t:texture2D");
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (!assetPath.Contains(path))
+                    continue;
+
+                Texture2D asset = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
+                if (asset != null)
+                    assets.Add(asset);
+            }
+
+            return assets.ToArray();
+        }
+
+
+        public static T[] FindAllAt<T>(string path = "") where T : Object
+        {
+            List<T> assets = new List<T>();
+            string[] guids = AssetDatabase.FindAssets(typeMap[typeof(T)]);
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (!assetPath.Contains(path))
+                    continue;
+
+                T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                if (asset != null)
+                    assets.Add(asset);
+            }
+
+            return assets.ToArray();
+        }
+
+
+        public static string[] FindMatchingAssets(string[] matches)
+        {
+            List<string> assets = new List<string>();
+            string[] guids = AssetDatabase.FindAssets("", null);
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+                for (int e = 0; e < matches.Length; e++)
+                    if (!assetPath.Contains(matches[e]))
+                        goto NoMatch;
+
+                assets.Add(assetPath);
                 continue;
 
-            Object[] allObjects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+                NoMatch: ;
+            }
 
-            for (int o = 0; o < allObjects.Length; o++)
-                if (allObjects[o] is Mesh)
-                {
-                    assets.Add((Mesh)allObjects[o]);
-                    used.Add(assetPath);
-                    
-                    if(!getAllInObject)
-                        break;
-                }
-                    
+
+            return assets.ToArray();
         }
-        return assets.ToArray();
-    }
 
 
-    public static T Get<T>(string name) where T : Object
-    {
-        string[] guids = AssetDatabase.FindAssets(name);
+        private static readonly Dictionary<Type, string> typeMap;
 
-        if (guids.Length == 0)
+        static Assets()
         {
-            Debug.Log("Couldn't find Asset with Name \"" + name + "\"");
-            return null;
+            typeMap = new Dictionary<Type, string>()
+            {
+                {typeof(Texture2D), "t:texture2D"},
+                {typeof(Material), "t:material"},
+                {typeof(TextAsset), "t:textasset"}
+            };
         }
-        
-        if (guids.Length > 1)
-        {
-            Debug.Log("Too many Assets with Name \"" + name + "\"");
-            return null;
-        }
-        
-        string guid = guids[0];
-        T thing = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
-
-        if (thing == null)
-            Debug.Log("Assets with Name \"" + name + "\" isn't the right Type");
-
-        return thing;
-    }
-    
-    
-    public static Material[] FindAllMaterials(string path = "")
-    {
-        List<Material> assets = new List<Material>();
-        string[] guids = AssetDatabase.FindAssets("t:material");
-        
-        for( int i = 0; i < guids.Length; i++ )
-        {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-            if(!assetPath.Contains(path))
-                continue;
-
-            Material asset = AssetDatabase.LoadAssetAtPath<Material>( assetPath );
-            if(asset != null)
-                assets.Add(asset);
-                    
-        }
-        return assets.ToArray();
-    }
-    
-    
-    public static Texture2D[] FindAllTextures(string path = "")
-    {
-        List<Texture2D> assets = new List<Texture2D>();
-        string[] guids = AssetDatabase.FindAssets("t:texture2D");
-        
-        for( int i = 0; i < guids.Length; i++ )
-        {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-            if(!assetPath.Contains(path))
-                continue;
-
-            Texture2D asset = AssetDatabase.LoadAssetAtPath<Texture2D>( assetPath );
-            if(asset != null)
-                assets.Add(asset);
-                    
-        }
-        return assets.ToArray();
-    }
-
-
-    public static T[] FindAllAt<T>(string path = "") where T : Object
-    {
-        List<T> assets = new List<T>();
-        string[] guids = AssetDatabase.FindAssets(typeMap[typeof(T)]);
-        
-        for( int i = 0; i < guids.Length; i++ )
-        {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-            if(!assetPath.Contains(path))
-                continue;
-
-            T asset = AssetDatabase.LoadAssetAtPath<T>( assetPath );
-            if(asset != null)
-                assets.Add(asset);
-                    
-        }
-        return assets.ToArray();
-    }
-    
-    
-    public static string[] FindMatchingAssets(string[] matches)
-    {
-        List<string> assets = new List<string>();
-        string[] guids = AssetDatabase.FindAssets("", null );
-      
-        for( int i = 0; i < guids.Length; i++)
-        {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-
-            for (int e = 0; e < matches.Length; e++)
-                if(!assetPath.Contains(matches[e]))
-                    goto NoMatch;
-            
-            assets.Add(assetPath);
-            continue;
-            
-            NoMatch:;
-        }
-        
-        
-        return assets.ToArray();
-    }
-
-
-    private static readonly Dictionary<Type, string> typeMap;
-    
-    static Assets()
-    {
-        typeMap = new Dictionary<Type, string>()
-        {
-            {typeof(Texture2D), "t:texture2D"},
-            {typeof(Material),  "t:material"},
-            {typeof(TextAsset), "t:textasset"}
-        };
     }
 }
 
